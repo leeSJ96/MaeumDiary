ec4af608e6f32c8035bff7ea0ae34de50e898e9e
Administrator <skwk55555@naver.com>
2020-12-11 23:01:17 +0900

    프래그먼트 생성, 파이어베이스 연결

---
+++ app/src/main/java/com/poly/test/diaryapp/Home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.poly.test.diaryapp.Home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.poly.test.diaryapp.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
--- app/src/main/java/com/poly/test/diaryapp/MainActivity.kt
+++ app/src/main/java/com/poly/test/diaryapp/MainActivity.kt
@@ -2,10 +2,57 @@ package com.poly.test.diaryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.poly.test.diaryapp.Calendar.CalendarFragment
+import com.poly.test.diaryapp.Home.HomeFragment
+import com.poly.test.diaryapp.Setting.SettingFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        supportFragmentManager.beginTransaction().add(R.id.frag_layout, homeFragment).commit()
+
+        menu_bar.selectedItemId = R.id.home_tab
+
+        menu_bar.setOnNavigationItemSelectedListener(this)
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            R.id.home_tab -> {
+                val homeFragment = HomeFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frag_layout, homeFragment)
+                    .commit()
+                return true
+            }
+
+            R.id.calendar_tab -> {
+                val calendarFragment = CalendarFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frag_layout, calendarFragment)
+                    .commit()
+                return true
+            }
+
+            R.id.setting_tab -> {
+                val settingFragment = SettingFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frag_layout, settingFragment)
+                    .commit()
+                return true
+            }
+
+        }
+
+        return true
+    }
+
+
+
 }
